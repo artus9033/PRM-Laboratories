3f9acf7b81e0bbd4b59fc5be05f5555a7da1ef6a
artus9033 <artus9033@gmail.com>
2023-10-13 12:58:01 +0200

    Added a suggestion for confirmation of cleanup

--- lab1/cleanup.bash
+++ lab1/cleanup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Czy na pewno chcesz przywrócić stan początkowy laboratorium? To usunie wszystkie pliki. " -n 1 -r
+read -p "Czy na pewno chcesz przywrócić stan początkowy laboratorium? To usunie wszystkie pliki. [Y/N] " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -10,6 +10,6 @@ fi
 cd ~/Desktop/PRM-Laboratories/lab1
 git restore . # restore original lab contents
 docker stop $(docker ps -a -q) # stop all running containers
-docker container prune -f # stop all stopped containers
+docker container prune -f # remove all stopped containers
 docker image rm http-server ros-turtle # remove result images built by students during the lab
 history -c && history -w # clear terminal history
